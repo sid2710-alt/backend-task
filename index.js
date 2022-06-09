c057d611a4ef60db9f2ce5264a5441e0125a00e4
sid2710-alt <73985972+sid2710-alt@users.noreply.github.com>
2022-06-09 12:17:17 +0530

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 app.use(cors());
-const port =  3000;
+const port =  process.env.PORT || 3000;
 const db = require('./config/mongoose');
 
 app.use(express.urlencoded({ extended: true }));
